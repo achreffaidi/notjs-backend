1b0afe9aad8805c2110e9857f49cd8be138ff822
SaidFayache <Said.fayache@gmail.com>
2020-12-22 21:38:33 +0100

    login + token

---
+++ src/users/dto/user-login.dto.ts
@@ -0,0 +1,9 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class UserLoginDto {
+  @IsNotEmpty()
+  @IsEmail()
+  readonly email: string;
+  @IsNotEmpty()
+  readonly password: string;
+}
---
+++ src/users/strategy/Passport.jwt.strategy.ts
@@ -0,0 +1,31 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UnauthorizedException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from '../schemas/user.schema';
+import { Model } from 'mongoose';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+
+export class PassportJwtStrategy extends PassportStrategy(Strategy) {
+
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.SECRET_KEY
+    });
+  }
+
+  async validate(payload) {
+    const user = await this.userModel.findOne({email: payload.email});
+    if (user) {
+      delete user.password;
+      return user; //Add what u want to be returned in ny request user.id for exple
+    } else {
+      throw new UnauthorizedException();
+    }
+  }
+}
